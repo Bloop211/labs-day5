a00dbb7fcb10e3a7f8a8064bd4b1298ec2d46cdc
Nikhil <nsrao2640@gmail.com>
2025-07-07 13:51:33 -0500

    filmed base weather vid

--- main.py
+++ main.py
@@ -7,13 +7,11 @@ cities = {
 }
 select_city = input("Select a city (Chicago, New York, Los Angeles): ").lower()
 
-weather_check_url = (f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true")
-geocode_url = (f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true")
-
 if select_city in cities:
     lat = cities[select_city]["lat"]
     lon = cities[select_city]["lon"]
-    response = httpx.get(weather_check_url)
+    url = (f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true")
+    response = httpx.get(url)
     weather = response.json()
     temperature = weather["current_weather"]["temperature"]
     windspeed = weather["current_weather"]["windspeed"]
