6753dfa091bd681199e446912f0235f81e2d81fe
Nikhil <nsrao2640@gmail.com>
2025-07-08 11:23:01 -0500

    updated lamp stretch

--- smartlamp_objects.py
+++ smartlamp_objects.py
@@ -6,12 +6,25 @@ class Smartlamp:
     color: str = "white"
     brightness: int = 100
     is_on: bool = field(default=False, init=False)
+    _registry = []
+    
+    def __post_init__(self):
+        self._registry.append(self)
+    
+    @classmethod
+    def power_outage(cls):
+        for lamp in cls._registry:
+            lamp.is_on = False
+        print("All lamps have been turned off (power outage).")
         
     def turn_on(self):
         self.is_on = True
 
     def turn_off(self):
         self.is_on = False
+
+    def toggle(self):
+        self.is_on = not self.is_on
 
     def set_brightness(self, percent):
         if percent > 100 or percent < 0:
@@ -24,20 +37,17 @@ class Smartlamp:
         
     def status(self):
         return (f"{self.name} is {'on' if self.is_on else 'off'}, {self.color}, and {self.brightness}% bright")
-    
-    def power_outage(self):
-        self.is_on = False
-        print(f"{self.name} has been turned off due to power outage")
 
 if __name__ == "__main__":
-    lamp = Smartlamp("Lamp 1")
-    lamp.is_on = not lamp.is_on
-    lamp.set_brightness(50)
-    lamp.set_color("grey")
-    print(lamp.status())
-    lamp = Smartlamp("Lamp 2")
-    lamp.is_on = not lamp.is_on
-    lamp.set_brightness(20)
-    lamp.set_color("black")
-    print(lamp.status())
-    lamp.power_outage()
+    lamp1 = Smartlamp("Lamp 1")
+    lamp1.set_brightness(50)
+    lamp1.set_color("grey")
+    print(lamp1.status())
+    lamp2 = Smartlamp("Lamp 2")
+    lamp2.toggle()
+    lamp2.set_brightness(20)
+    lamp2.set_color("black")
+    print(lamp2.status())
+    Smartlamp.power_outage()
+    print(lamp1.status())
+    print(lamp2.status())
